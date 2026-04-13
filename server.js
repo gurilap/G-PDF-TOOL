f520204d581cd1e49d4b5627e75136b549a5261b
gurilap <s13gurdita@gmail.com>
2026-04-13 08:50:16 +0530

    v3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'index.html')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
